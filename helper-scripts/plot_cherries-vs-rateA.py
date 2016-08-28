4fb518fb4e71c3251579d5540a69b0cd04a2b3e6
Niema Moshiri <niemamoshiri@gmail.com>
2016-08-28 14:48:30 -0700

    Add gitignore and a helper script

---
+++ helper-scripts/plot_cherries-vs-rateA.py
@@ -0,0 +1,8 @@
+'''
+Niema Moshiri 2016
+Generate plots of number of cherries vs. rateA (activation rate)
+'''
+# imports
+import sys
+sys.path.insert(0,'../tools')
+from AluSimulator import simulateAlu
