16cafa2ed3f174e982e9dce1efa509bf98e38d6c
Niema Moshiri <niemamoshiri@gmail.com>
2016-10-15 19:03:37 -0700

    Rewrote numlist in C++ and moved to tools repo

--- helper-scripts/calc_all_cherry_dev.sh
+++ helper-scripts/calc_all_cherry_dev.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# WARNING: This script needs numlist, which is a program located in my "tools" GitHub repo
 # USAGE: Just run this script in the directory containing all of the tree parameter folders
 for d in $(seq -w 0 24); do
     # do fasttree fixes (fasttree support values are decimals, so do 0-1)
@@ -6,8 +7,8 @@ for d in $(seq -w 0 24); do
     echo -n `for thresh in $(seq 0 0.01 1.01); do
         for tree in param-$d*/*fasttree*/*.tre; do
             ~/GitHub/Alu-Project/tools/estimate-cherries.sh $tree $thresh 2>/dev/null
-        done | ~/GitHub/Alu-Project/helper-scripts/numlist_math.py -avg
-    done | ~/GitHub/Alu-Project/helper-scripts/numlist_math.py -csv`
+        done | ~/GitHub/Alu-Project/helper-scripts/numlist -avg
+    done | ~/GitHub/Alu-Project/helper-scripts/numlist -csv`
     echo "]"
 
     # do raxml fixes (raxml support values are percentages, so do 0-100)
@@ -15,8 +16,8 @@ for d in $(seq -w 0 24); do
     echo -n `for thresh in $(seq 0 100); do
         for tree in param-$d*/*raxml*/*support*.tre; do
             ~/GitHub/Alu-Project/tools/estimate-cherries.sh $tree $thresh 2>/dev/null
-        done | ~/GitHub/Alu-Project/helper-scripts/numlist_math.py -avg
-    done | ~/GitHub/Alu-Project/helper-scripts/numlist_math.py -csv`
+        done | ~/GitHub/Alu-Project/helper-scripts/numlist -avg
+    done | ~/GitHub/Alu-Project/helper-scripts/numlist -csv`
     echo "]"
 
     # empty line between parameters
--- helper-scripts/compute_tree_stats.sh
+++ helper-scripts/compute_tree_stats.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# WARNING: This script needs numlist, which is a program located in my "tools" GitHub repo
 
 # compute tree stats on all trees in the current working directory (*.tre.gz files)
 # so far, only does fast stats:
@@ -16,5 +17,5 @@ for tree in *.tre.gz; do
 
     # compute average branch length
     echo -n "Average Branch Length: " >> $treFile.stats
-    gunzip -c $tree | nw_distance -mp -sa - | ~/GitHub/Alu-Project/helper-scripts/numlist_math.py -avg >> $treFile.stats
+    gunzip -c $tree | nw_distance -mp -sa - | ~/GitHub/Alu-Project/helper-scripts/numlist -avg >> $treFile.stats
 done
