807521045f5716da45d9aae16a955839d864a51a
Siavash Mirarab <smirarab@gmail.com>
2016-10-17 09:19:31 -0700

    min update

--- tools/supported-subtrees-min.py
+++ tools/supported-subtrees-min.py
@@ -47,16 +47,16 @@ if __name__ == '__main__':
                    if child.c > 0:
                        node.n += child.n
                        node.c += child.c
-                   node.m += child.m
+                       node.m += child.m
                 ext = -1 if node.n == 0 else (node.c+0.0)/node.n 
                 ss = node.n
                 for child in node.child_nodes():
                     if child.c == 0:
                         node.n += child.n
                         node.c += 0 if ext == -1 or child.n == 1  else max(ext*child.n,child.m)
-                        #node.m += max(ext*child.n,child.m)
+                        node.m += 0 if ext == -1 or child.n == 1  else max(ext*child.n,child.m)
                 node.m = max (node.m, 1)
                 #node.c = (node.c+0.0)/nsum*(node.n-cleaves) if nsum > 0 else 0
             #if node.c != node.m:
-            #    print("%d %d %d %s" %(node.n,node.c,node.m,node._as_newick_string(suppress_leaf_taxon_labels=True,suppress_leaf_node_labels =True,suppress_internal_taxon_labels=True,suppress_internal_node_labels=True,suppress_annotations=True,suppress_edge_lengths=True)))
+        print("%d %d %d\n%s" %(node.n,node.c,node.m,tree.as_string(schema='newick',suppress_leaf_taxon_labels=True,suppress_leaf_node_labels =True,suppress_internal_taxon_labels=True,suppress_internal_node_labels=True,suppress_annotations=True,suppress_edge_lengths=True)))
         print ((tree.seed_node.c+0.0)/tree.seed_node.n)
