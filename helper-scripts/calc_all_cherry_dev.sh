9c38839e9c14cd31c4c39a75c030c23f8186c55c
Niema Moshiri <niemamoshiri@gmail.com>
2016-10-16 17:27:30 -0700

    Compute values using supported-subtrees-min.py as well

--- helper-scripts/calc_all_cherry_dev.sh
+++ helper-scripts/calc_all_cherry_dev.sh
@@ -4,25 +4,57 @@
 
 command -v numlist >/dev/null 2>&1 || { echo >&2 "ERROR: numlist not found in PATH. Get numlist from my \"tools\" GitHub repo (niemasd/tools)."; exit 1; }
 
+# using estimate-cherries.sh
+echo "y_estimate_cherries_sh = {"
+echo "    # fasttree values"
 for d in $(seq -w 0 24); do
     # do fasttree fixes (fasttree support values are decimals, so do 0-1)
-    echo -n "param$d" && echo -n "_fasttree = ["
+    echo -n "    'param$d" && echo -n "_fasttree':["
     echo -n `for thresh in $(seq 0 0.01 1.01); do
         for tree in param-$d*/*fasttree*/*.tre; do
             ~/GitHub/Alu-Project/tools/estimate-cherries.sh $tree $thresh 2>/dev/null
-        done | ~/GitHub/Alu-Project/helper-scripts/numlist -avg
-    done | ~/GitHub/Alu-Project/helper-scripts/numlist -csv`
-    echo "]"
-
+        done | numlist -avg
+    done | numlist -csv`
+    echo "],"
+done
+echo ""
+echo "    # RAxML values"
+for d in $(seq -w 0 24); do
     # do raxml fixes (raxml support values are percentages, so do 0-100)
-	echo -n "param$d" && echo -n "_raxml = ["
+    echo -n "    'param$d" && echo -n "_raxml':["
     echo -n `for thresh in $(seq 0 100); do
         for tree in param-$d*/*raxml*/*support*.tre; do
             ~/GitHub/Alu-Project/tools/estimate-cherries.sh $tree $thresh 2>/dev/null
-        done | ~/GitHub/Alu-Project/helper-scripts/numlist -avg
-    done | ~/GitHub/Alu-Project/helper-scripts/numlist -csv`
-    echo "]"
-
-    # empty line between parameters
-    echo ""
+        done | numlist -avg
+    done | numlist -csv`
+    echo "],"
 done
+echo "}"
+echo ""
+
+# using supported-subtrees-min.py
+echo "y_supported_subtrees_min_py = {"
+echo "    # fasttree values"
+for d in $(seq -w 0 24); do
+    # do fasttree fixes (fasttree support values are decimals, so do 0-1)
+    echo -n "    'param$d" && echo -n "_fasttree':["
+    echo -n `for thresh in $(seq 0 0.01 1.01); do
+        for tree in param-$d*/*fasttree*/*.tre; do
+            ~/GitHub/Alu-Project/tools/supported-subtrees-min.py $tree $thresh
+        done | numlist -avg
+    done | numlist -csv`
+    echo "],"
+done
+echo ""
+echo "    # RAxML values"
+for d in $(seq -w 0 24); do
+    # do raxml fixes (raxml support values are percentages, so do 0-100)
+    echo -n "    'param$d" && echo -n "_raxml':["
+    echo -n `for thresh in $(seq 0 100); do
+        for tree in param-$d*/*raxml*/*support*.tre; do
+            ~/GitHub/Alu-Project/tools/supported-subtrees-min.py $tree $thresh 2>/dev/null
+        done | numlist -avg
+    done | numlist -csv`
+    echo "],"
+done
+echo "}"
