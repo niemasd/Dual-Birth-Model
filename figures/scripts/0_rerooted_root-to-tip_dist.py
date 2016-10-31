53ed7e300cef0a3a813b98e9b90086baf080af2c
Niema Moshiri <niemamoshiri@gmail.com>
2016-10-31 10:58:33 -0700

    more figures

---
+++ figures/scripts/0_rerooted_root-to-tip_dist.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+'''
+Niema Moshiri 2016
+
+Generate histograms of Alu tree root-to-tip distances after rerooting on best
+estimate of 7SLRNA MRCA
+'''
+# imports
+from matplotlib import rcParams
+from matplotlib.patches import Patch
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import sys
+
+# settings
+sns.set_style("ticks")
+rcParams['font.family'] = 'serif'
+if len(sys.argv) < 2:
+    print("ERROR: Incorrect number of arguments")
+    print("USAGE: python 0_rerooted_root-to-tip_dist.py <data>")
+    exit(-1)
+data = eval(open(sys.argv[1]).read())
+
+# create histogram
+fig = plt.figure()
+sns.distplot(data, hist_kws={'weights':np.ones_like(data)/float(len(data))}, kde=False)
+sns.plt.title("Root-to-Tip Distance", fontsize=18, y=1.05)
+sns.plt.xlabel("Root-to-Tip Distance", fontsize=14)
+sns.plt.ylabel("Frequency", fontsize=14)
+sns.plt.show()
+fig.savefig(sys.argv[1] + '_rerooted_root-to-tip_dist.pdf', format='pdf', bbox_inches='tight')
+plt.close()
