665fa0991b470bf1f04e27624b820e54f1928345
Niema Moshiri <niemamoshiri@gmail.com>
2016-11-19 15:19:57 -0800

    Updated figures

--- figures/scripts/0_rerooted_numkids.py
+++ figures/scripts/0_rerooted_numkids.py
@@ -21,17 +21,17 @@ if len(sys.argv) < 2:
     print("ERROR: Incorrect number of arguments")
     print("USAGE: python 0_rerooted_root-to-tip_dist.py <data>")
     exit(-1)
-y = sorted([int(i.strip().split(',')[1]) for i in open(sys.argv[1]).read().strip().splitlines()])
+y = sorted([int(i.strip()) for i in open(sys.argv[1]).read().strip().splitlines()])
 x = [i for i in range(len(y))]
 
 # create histogram
 fig = plt.figure()
 plt.plot(x,y)
-plt.plot([-1000,8000],[446,446],linestyle='--',color='#000000')
+plt.plot([-1000,8000],[444,444],linestyle='--',color='#000000')
 plt.xlim(0,len(x)+1)
 sns.plt.title("Number of 7SLRNA Descendants", fontsize=18, y=1.05)
 sns.plt.xlabel(r"Node $u$", fontsize=14)
 sns.plt.ylabel(r"Number of 7SLRNA Descendants of Node $u$", fontsize=14)
 sns.plt.show()
-fig.savefig('0_rerooted_numkids.pdf', format='pdf', bbox_inches='tight')
+fig.savefig(sys.argv[1] + '_rerooted_numkids.pdf', format='pdf', bbox_inches='tight')
 plt.close()
