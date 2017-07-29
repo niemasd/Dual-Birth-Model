de4c5d2882cb789decf711a4fa00266a6e361bc1
Siavash Mirarab <smirarab@gmail.com>
2017-07-29 11:14:49 -0700

    update to not rely on node conventions

--- tools/estimate-r-by-bl.sh
+++ tools/estimate-r-by-bl.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-tmp=`mktemp $TMPDIR/R"XXXX"`
-nw_distance -n -sa -mp -|sed -e "s/^I/I /" -e "s/^L/L /"|awk '{print $3,$1}' > $tmp
+tmpa=`mktemp XXXX`
+tmpf=`mktemp XXXX`
+
+cat - | tee >( nw_distance -sf -mp - > $tmpf )| nw_distance -sa -mp - >$tmpa
 
 echo "
-tmp=read.csv('"$tmp"',sep= ' ',header=F)
-attach(tmp)
-l = mean(tmp[V2=='L', 1])
-b = mean(V1)
-if ( b > l) {
-	cat( 1 )
-} else {
-	cat(   (-2 * b^3+5 * b^2* l-3 *b* l^2+2 * sqrt(2) * sqrt(-b^2* (b-l)^3 * l))/(b * (b-l) * l) )
-}
+l = mean(read.csv('"$tmpf"',sep= ' ',header=F)\$V1)
+b = mean(read.csv('"$tmpa"',sep= ' ',header=F)\$V1)
+cat( if ( b > l) 1 else  (-2 * b^3+5 * b^2* l-3 *b* l^2+2 * sqrt(2) * sqrt(-b^2* (b-l)^3 * l))/(b * (b-l) * l) )
 " | R --vanilla -q --slave
 
 echo
+
+rm $tmpf
+rm $tmpa
