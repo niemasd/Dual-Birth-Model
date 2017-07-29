04bf723ac64468496a84adf2a58c59197c20c66c
Siavash Mirarab <smirarab@gmail.com>
2017-07-29 00:02:19 -0700

    corner case

--- tools/estimate-r-by-bl.sh
+++ tools/estimate-r-by-bl.sh
@@ -8,7 +8,11 @@ tmp=read.csv('"$tmp"',sep= ' ',header=F)
 attach(tmp)
 l = mean(tmp[V2=='L', 1])
 b = mean(V1)
-cat(   (-2 * b^3+5 * b^2* l-3 *b* l^2+2 * sqrt(2) * sqrt(-b^2* (b-l)^3 * l))/(b * (b-l) * l) )
+if ( b > l) {
+	cat( 1 )
+} else {
+	cat(   (-2 * b^3+5 * b^2* l-3 *b* l^2+2 * sqrt(2) * sqrt(-b^2* (b-l)^3 * l))/(b * (b-l) * l) )
+}
 " | R --vanilla -q --slave
 
 echo
