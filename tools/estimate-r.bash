c7c1b33b076e4e3eac7e4c1c0cc830d0aa08d80d
Siavash Mirarab <smirarab@gmail.com>
2017-07-28 23:55:21 -0700

    add new estimator

---
+++ tools/estimate-r.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+tmp=`mktemp $TMPDIR/R"XXXX"`
+nw_distance -n -sa -mp -|sed -e "s/^I/I /" -e "s/^L/L /"|awk '{print $3,$1}' > $tmp
+
+echo "
+tmp=read.csv('"$tmp"',sep= ' ',header=F)
+attach(tmp)
+l = mean(tmp[V2=='L', 1])
+b = mean(V1)
+cat(   (-2 * b^3+5 * b^2* l-3 *b* l^2+2 * sqrt(2) * sqrt(-b^2* (b-l)^3 * l))/(b * (b-l) * l) )
+" | R --vanilla -q --slave
+
+echo
